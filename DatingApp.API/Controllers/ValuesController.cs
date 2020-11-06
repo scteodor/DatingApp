5c6bde5783d0e1293cd58714f6f0e71d1addf0b8
Stefan Teodor <stefan.constantin.teodor@gmail.com>
2020-11-06 17:39:56 +0200

    Added login and register forms

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -24,6 +24,7 @@ namespace DatingApp.API.Controllers
             _logger = logger;
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
