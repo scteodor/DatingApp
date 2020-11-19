0d86b28c32eb30ef4963ce31d5d26c86b9cd676a
Stefan Teodor <stefan.constantin.teodor@gmail.com>
2020-11-19 22:52:37 +0200

    Implemented likes

--- DatingApp.API/Helpers/UserParams.cs
+++ DatingApp.API/Helpers/UserParams.cs
@@ -14,9 +14,11 @@ namespace DatingApp.API.Helpers
         public int UserId { get; set; }
         public string Gender { get; set; }
         public int MinAge { get; set; } = 18;
-
         public int MaxAge { get; set; } = 99;
 
         public string OrderBy { get; set; }
+
+        public bool Likees { get; set; }
+        public bool Likers { get; set; }
     }
 }
